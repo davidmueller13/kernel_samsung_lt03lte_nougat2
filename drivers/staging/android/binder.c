ec118ff3962b99acf63816f5dacf1be8ff01a59c
Serban Constantinescu <serban.constantinescu@arm.com>
2013-07-04 10:54:46 +0100

    staging: android: binder: fix alignment issues
    The Android userspace aligns the data written to the binder buffers to
    4bytes. Thus for 32bit platforms or 64bit platforms running an 32bit
    Android userspace we can have a buffer looking like this:
    
    platform    buffer(binder_cmd   pointer)      size
    32/32                 32b         32b          8B
    64/32                 32b         64b          12B
    64/64                 32b         64b          12B
    
    Thus the kernel needs to check that the buffer size is aligned to 4bytes
    not to (void *) that will be 8bytes on 64bit machines.
    
    The change does not affect existing 32bit ABI.
    
    Change-Id: Idcad35da1c1567ee0676d60d03afd07b219c59ea
    Signed-off-by: Serban Constantinescu <serban.constantinescu@arm.com>
    Acked-by: Arve Hjønnevåg <arve@android.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Git-commit: ec35e852dc9de9809f88ff397d7a611208880f9f
    Git-repo: https://android.googlesource.com/kernel/common/
    Signed-off-by: Neeti Desai <neetid@codeaurora.org>
    Signed-off-by: Ajay Dudani <adudani@codeaurora.org>
    (cherry picked from commit 34b7be94a99c849e509a3ba62f151b448eb9cb5d)
    
    # Conflicts:
    #	drivers/staging/android/binder.c

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -1335,7 +1335,7 @@ static void binder_transaction_buffer_release(struct binder_proc *proc,
 		struct flat_binder_object *fp;
 		if (*offp > buffer->data_size - sizeof(*fp) ||
 		    buffer->data_size < sizeof(*fp) ||
-		    !IS_ALIGNED(*offp, sizeof(void *))) {
+		    !IS_ALIGNED(*offp, sizeof(u32))) {
 			pr_err("transaction release %d bad offset %zd, size %zd\n",
 			 debug_id, *offp, buffer->data_size);
 			continue;
@@ -1591,8 +1591,8 @@ static void binder_transaction(struct binder_proc *proc,
 		if (*offp > t->buffer->data_size - sizeof(*fp) ||
 		    *offp < off_min ||
 		    t->buffer->data_size < sizeof(*fp) ||
-		    !IS_ALIGNED(*offp, sizeof(void *))) {
-			binder_user_error("binder: %d:%d got transaction with "
+		    !IS_ALIGNED(*offp, sizeof(u32))) {
+			binder_user_error(" %d:%d got transaction with "
 				proc->pid, thread->pid, *offp, (u32)off_min,
 				(u32)(t->buffer->data_size - sizeof(*fp)));
 			return_error = BR_FAILED_REPLY;
